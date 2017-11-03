ab6ecf2e1d963b0445169107e9d7cc5b63b8d5fa
Lukas Pustina <lukas@pustina.net>
2017-11-03 15:31:10 +0100

    Add service and tag filter for sw-discover

--- src/bin/sw-discover.rs
+++ src/bin/sw-discover.rs
@@ -1,23 +1,41 @@
+extern crate clap;
 extern crate consul;
 extern crate tabwriter;
 
+use clap::{App, Arg};
 use consul::Client;
 use tabwriter::TabWriter;
+use std::collections::HashSet;
 use std::io::Write;
 
 fn main() {
-    let url = ::std::env::args().nth(1).unwrap_or_else(|| "http:://localhost:8500".to_string());
+    let args = build_cli().get_matches();
 
-    let client = Client::new(&url);
+    let url: &str = args.value_of("url").unwrap();
+    let service_filter: Option<Vec<_>> = args.values_of_lossy("services");
+    let tag_filter: Option<Vec<_>> = args.values_of_lossy("tags");
+
+    let client = Client::new(url);
     let mut tw = TabWriter::new(vec![]).padding(1);
 
     let services = client.catalog.services().unwrap();
     let mut service_names: Vec<_> = services.keys().collect();
     service_names.sort();
     for service in service_names {
+        if let Some(ref s_filter) = service_filter {
+            if !s_filter.contains(&service) {
+                continue;
+            }
+        }
         let nodes = client.catalog.get_nodes(service.clone()).unwrap();
         let _ = writeln!(&mut tw, "Service '{}' tagged with {:?}", service, services.get(service).unwrap());
         for node in nodes {
+            if let Some(ref s_tags) = tag_filter {
+                let set: HashSet<_> = s_tags.iter().chain(node.ServiceTags.iter()).collect();
+                if set.len() == s_tags.len() + node.ServiceTags.len() {
+                    continue;
+                }
+            }
             let _ = writeln!(&mut tw, "\t* Node '{}'\tip:{},\tservice: id:{},\tname:{},\tport:{},\ttags:{:?}", node.Node, node.Address, node.ServiceID, node.ServiceName, node.ServicePort, node.ServiceTags );
         }
         let _ = writeln!(&mut tw, "");
@@ -25,5 +43,54 @@ fn main() {
 
     let out_str = String::from_utf8(tw.into_inner().unwrap()).unwrap();
     print!("{}", out_str);
+}
+
+fn build_cli() -> App<'static, 'static> {
+    let name = "sw-discover";
+    let version = env!("CARGO_PKG_VERSION");
+
+    App::new(name)
+        .version(version)
+        .arg(
+            Arg::with_name("url")
+                .index(1)
+                .required(true)
+                .conflicts_with("completions")
+                .help("URL of consul agent to retrieve catalog from")
+        )
+        .arg(
+            Arg::with_name("services")
+                .value_name("service name")
+                .long("service")
+                .short("s")
+                .takes_value(true)
+                .multiple(true)
+                .require_delimiter(true)
+                .number_of_values(1)
+                .help(
+                    "Filters service for specified service names"
+                )
+        )
+        .arg(
+            Arg::with_name("tags")
+                .value_name("tag name")
+                .long("tag")
+                .short("t")
+                .takes_value(true)
+                .multiple(true)
+                .require_delimiter(true)
+                .number_of_values(1)
+                .help(
+                    "Filters service for specified tags"
+                )
+        )
+        .arg(
+            Arg::with_name("completions")
+                .long("completions")
+                .takes_value(true)
+                .hidden(true)
+                .possible_values(&["bash", "fish", "zsh"])
+                .help("The shell to generate the script for")
+        )
 }
 
