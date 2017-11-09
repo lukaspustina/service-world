9ca26063877ca735ea0daaf12200d59ea2b196eb
Lukas Pustina <lukas@pustina.net>
2017-11-09 22:16:50 +0100

    Add Service URLs to sw-present

--- src/bin/sw-present.rs
+++ src/bin/sw-present.rs
@@ -2,16 +2,92 @@
 extern crate error_chain;
 extern crate clap;
 extern crate handlebars;
+extern crate serde;
 #[macro_use]
 extern crate serde_derive;
 extern crate service_world;
+extern crate toml;
 
 use clap::{App, Arg};
 use handlebars::{Handlebars, Helper, RenderContext, RenderError};
 use service_world::Consul;
+use std::collections::HashMap;
 use std::io::Write;
+use std::path::Path;
+
+mod config {
+    use std::collections::HashMap;
+    use std::fs::File;
+    use std::io::Read;
+    use std::path::Path;
+    use toml;
+
+    #[derive(Debug, Deserialize, Serialize)]
+    pub struct Config {
+        pub general: General,
+        pub consul: Consul,
+        pub services: HashMap<String, Vec<Service>>,
+    }
+
+    #[derive(Debug, Deserialize, Serialize)]
+    pub struct General {
+        pub project_name: String,
+    }
+
+    #[derive(Debug, Deserialize, Serialize)]
+    pub struct Consul {
+        pub urls: Vec<String>,
+    }
+
+    #[derive(Debug, Deserialize, Serialize)]
+    pub struct Service {
+        pub name: String,
+        pub url: String,
+    }
+
+    impl Default for Config {
+        fn default() -> Config {
+            let general = General { project_name: "Service World".to_string() };
+            let consul = Consul { urls: vec!["http://localhost:8500".to_string()] };
+            let services = HashMap::new();
+
+            Config { general, consul, services }
+        }
+    }
+
+    impl Config {
+        pub fn from_file(file_path: &Path) -> Result<Config> {
+            let mut file = File::open(file_path)?;
+            let content = Config::read_to_string(&mut file)?;
+
+            Config::parse_toml(&content)
+        }
+
+        fn read_to_string(file: &mut File) -> Result<String> {
+            let mut content = String::new();
+            file.read_to_string(&mut content)?;
+
+            Ok(content)
+        }
+
+        fn parse_toml(content: &str) -> Result<Config> {
+            let config: Config = toml::from_str(content)?;
+
+            Ok(config)
+        }
+    }
+
+    error_chain! {
+        foreign_links {
+            CouldNotRead(::std::io::Error);
+            CouldNotParse(::toml::de::Error);
+        }
+    }
+}
 
 fn run() -> Result<()> {
+    use config::Config;
+
     let args = build_cli().get_matches();
 
     let url = args.value_of("url").ok_or_else(|| {
@@ -20,6 +96,11 @@ fn run() -> Result<()> {
     let template_file = args.value_of("template").ok_or_else(|| {
         ErrorKind::CliError("Template not specified".to_string())
     })?;
+    let config = if let Some(config_file) = args.value_of("config") {
+        Config::from_file(Path::new(config_file))
+    } else {
+        Ok(Default::default())
+    }.unwrap();
 
     let consul = Consul::new(url.to_string());
     let catalog = consul.catalog()?;
@@ -33,12 +114,21 @@ fn run() -> Result<()> {
                 .into_iter()
                 .map(|node| {
                     let healthy = catalog.is_node_healthy_for_service(node, name);
+                    let mut service_urls = generate_service_ulrs(&config, name, node);
+                    let default_url = if let Some(ref mut s_urls) = service_urls {
+                        s_urls.remove("default")
+                    } else {
+                        None
+                    };
                     Node {
                         name: &node.name,
                         address: &node.address,
                         service_port: node.service_port,
                         service_tags: &node.service_tags,
-                        healthy }
+                        healthy,
+                        service_urls: service_urls,
+                        default_url: default_url
+                    }
                 })
                 .collect();
             let tags = catalog.service_tags(name).unwrap();
@@ -46,7 +136,7 @@ fn run() -> Result<()> {
         })
         .collect();
     services.sort_by_key(|x| x.name);
-    let context = Context { project_name: "Service World", services };
+    let context = Context { project_name: &config.general.project_name, services };
 
     render_template(template_file, &mut writer, &context)
 }
@@ -85,8 +175,27 @@ struct Node<'a> {
     service_port: u16,
     service_tags: &'a Vec<String>,
     healthy: bool,
+    service_urls: Option<HashMap<String, String>>,
+    default_url: Option<String>,
 }
 
+fn generate_service_ulrs(config: &config::Config, service_name: &str, node: &service_world::Node) -> Option<HashMap<String, String>> {
+    if let Some(services) = config.services.get(service_name) {
+        let mut m = HashMap::new();
+        let mut handlebars = Handlebars::new();
+
+        for service in services {
+            let template_name = format!("service_url-{}", service.name);
+            handlebars.register_template_string(&template_name, &service.url).unwrap();
+            let rendered_url = handlebars.render(&template_name, node).unwrap();
+            m.insert(service.name.to_string(), rendered_url);
+        }
+
+        Some(m)
+    } else {
+       None
+    }
+}
 
 fn render_template(template_file: &str, mut w: &mut Write, context: &Context) -> Result<()> {
     let mut handlebars = Handlebars::new();
@@ -110,6 +219,13 @@ fn build_cli() -> App<'static, 'static> {
                 .required(true)
                 .conflicts_with("completions")
                 .help("URL of consul agent to retrieve catalog from"),
+        )
+        .arg(
+            Arg::with_name("config")
+                .short("c")
+                .long("config")
+                .takes_value(true)
+                .help("Reads config file"),
         )
         .arg(
             Arg::with_name("template")
